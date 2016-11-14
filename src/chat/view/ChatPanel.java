cf9dedb0316054170b825bbd6be3556e6401e4ea
Jake Nilson <jnil0572@740s-pl-132.local>
2016-11-14 12:31:05 -0700

    Added Helper Methods and the sets for the buttons and other components.

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,6 +1,49 @@
 package chat.view;
 
-public class ChatPanel
-{
+import chat.controller.ChatController;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class ChatPanel extends JPanel
+{
+	private ChatController baseController;
+	private SpringLayout baseLayout;
+	private JTextArea chatDisplay;
+	private JTextField chatField;
+	private JButton chatButton;
+	
+	public ChatPanel(ChatController baseController)
+	{
+		super();
+		this.baseController = baseController;
+		baseLayout = new SpringLayout();
+		chatDisplay = new JTextArea(5, 25);
+		chatField = new JTextField(25);
+		chatButton = new JButton("Chat with the bot");
+		setupPanel();
+		setupLayout();
+		setupListeners();
+		
+	}
+	
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.setBackground(Color.GRAY);
+		this.add(chatButton);
+		this.add(chatField);
+		this.add(chatDisplay);
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
